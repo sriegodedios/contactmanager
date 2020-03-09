e38fe1fdfba1bba433ef34c571b5d044478ad706
sriegodedios <shaner261996@gmail.com>
2020-03-09 13:44:35 -0500

    stuff
    Added stuff

--- contactmanager/urls.py
+++ contactmanager/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('login/', include('login.urls')),
     path('register/', include('register.urls')),
+
 ]
