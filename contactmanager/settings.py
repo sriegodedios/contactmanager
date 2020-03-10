940c32b1a44a02486fc9b03151bc293b512c8d27
Krishane Riegodedios <krishane.riegodedios@hrblock.com>
2020-03-09 21:32:41 -0500

    Added templates to registration

--- contactmanager/settings.py
+++ contactmanager/settings.py
@@ -123,6 +123,21 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
 ]
-
-
 STATIC_URL = '/static/'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'register/templates'),
+                 ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
--- register/views.py
+++ register/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 
-def index(request):
-    return HttpResponse('Registration Page Goes Here')
+def index(req):
+    #return HttpResponse('Registration Page Goes Here')
+    return render(req, 'register/index.html')
